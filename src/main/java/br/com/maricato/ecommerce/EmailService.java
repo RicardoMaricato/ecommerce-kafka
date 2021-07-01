7cfc4c2611fbd11a2ac5e8935db5e44361cdd017
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-07-01 14:47:14 -0300

    -Custom deserialization

--- src/main/java/br/com/maricato/ecommerce/EmailService.java
+++ src/main/java/br/com/maricato/ecommerce/EmailService.java
@@ -2,17 +2,22 @@ package br.com.maricato.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
+
 public class EmailService {
 
     public static void main(String[] args) {
         var emailService = new EmailService();
-        var service = new KafkaService(EmailService.class.getSimpleName(),
+        try (var service = new KafkaService(EmailService.class.getSimpleName(),
                 "ECOMMERCE_SEND_EMAIL",
-                emailService::parse);
-        service.run();
+                emailService::parse,
+                String.class,
+                Map.of())) {
+            service.run();
+        }
     }
 
-    private void parse(ConsumerRecord<String,String> record) {
+    private void parse(ConsumerRecord<String, String> record) {
         System.out.println("------------------------------------------");
         System.out.println("Send email");
         System.out.println(record.key());
